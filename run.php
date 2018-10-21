926a1c7023b74759785da5b8daf774dbf76e2aa4
lukasztecza <lukasztecza.pl@gmail.com>
2018-10-20 21:01:28 -0400

    remove garbage

--- run.php
+++ run.php
@@ -1,8 +1,8 @@
 <?php
 // graph data settings
-$initialPrice = $price = 1000;
-$itertions = 5000;
-$priceChangeFactor = 20;
+$initialPrice = $price = 150;
+$itertions = 150;
+$priceChangeFactor = 10;
 $priceFluctuation = 5;
 $dateString = '2017-01-01 00:00:00';
 $values = [];
@@ -14,7 +14,6 @@ for ($i = 0; $i < $itertions; $i++) {
     $time = date('Y-m-d H:i:s', strtotime($dateString . ' +15min'));
     $open = $price;
     $close = $price + rand(-$priceChangeFactor, $priceChangeFactor);
-//    $close = $price + rand(-$priceChangeFactor, $priceChangeFactor +1 /* temp hack */);
     $high = $open < $close ? $close + rand(0, $priceFluctuation) : $open + rand(0, $priceFluctuation);
     $low = $open < $close ? $open - rand(0, $priceFluctuation) : $close - rand(0, $priceFluctuation);
     $values[] = [
@@ -37,7 +36,7 @@ echo 'Simulation end: ' . $dateString . PHP_EOL;
 // find local edge values and draw a chart
 $range = 10;
 $initialIndent = 100;
-$display = false;
+$display = true;
 $edgeValues = [];
 foreach ($values as $key => $value) {
     $scoreMax = 0;
@@ -66,17 +65,20 @@ foreach ($values as $key => $value) {
         for ($i = 1; $i < $initialIndent + $indentChangeLow; $i++) {
             echo ' ';
         }
+        $price_bar = "";
         for ($i = $i; $i <= $initialIndent + $indentChangeHigh; $i++) {
-            echo '.';
+            $price_bar .= '=';
         }
-        echo '               ';
+        $price_bar = substr($price_bar, 1, -1);
+        echo '|' . $price_bar . '|';
+        echo '       ';
         echo $value['average'];
     }
 
     // mark and store edge values
     if ($scoreMax === 2 * $range + 1) {
         if ($display) {
-            echo ' LOCAL MAX ';
+            echo ' || LOCAL MAX ';
             echo $value['high'];
         }
         $edgeValues[] = $value;
@@ -84,21 +86,24 @@ foreach ($values as $key => $value) {
     }
     if ($scoreMin === 2 * $range + 1) {
         if ($display) {
-            echo ' LOCAL MIN ';
+            echo ' || LOCAL MIN ';
             echo $value['low'];
         }
         $edgeValues[] = $value;
         $values[$key]['extreme'] = 'localMin';
     }
     if ($display) {
+//        echo ' || ' . $value['time'];
         echo PHP_EOL;
     }
 }
-//exit;
 
 //var_dump($values);exit;
+
 // simulate transaction ratios
 $ratioValues = [0.1, 0.33, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 10];
+$spread = 3;
+$size = 20;
 $ratios = [];
 foreach ($ratioValues as $ratioValue) {
     $index = count($ratios);
@@ -107,86 +112,34 @@ foreach ($ratioValues as $ratioValue) {
 $results = [];
 $initialWallet = 1000;
 echo 'Initial wallet: ' . $initialWallet . PHP_EOL;
-//$ratios = [3];
 foreach ($ratios as $ratio) {
     $trade = false;
     $direction = 0;
-    $size = 20;
     $wallet = $walletMin = $walletMax = $initialWallet;
     $lastLocalLow = $previousLocalLow = null;
     $lastLocalHigh = $previousLocalHigh = null;
     foreach ($values as $key => $value) {
-        if ($values[$key]['extreme'] === 'localMin') {
-            if ($lastLocalLow !== $previousLocalLow) {
-                $previousLocalLow = $lastLocalLow;
-            }
-            if ($lastLocalLow !== $value['low']) {
-                $lastLocalLow = $value['low'];
-            }
-        }
-
-        if ($values[$key]['extreme'] === 'localMax') {
-            if ($lastLocalHigh !== $previousLocalHigh) {
-                $previousLocalHigh = $lastLocalHigh;
-            }
-            if ($lastLocalHigh !== $value['high']) {
-                $lastLocalHigh = $value['high'];
-            }
-        }
-
+        // remember wallet min and max
         if ($walletMin > $wallet) {
             $walletMin = $wallet;
         }
         if ($walletMax < $wallet) {
             $walletMax = $wallet;
         }
+
         if (!$trade) {
-
-
-/*
-            if (!empty($values[$key - 1]) && !empty($values[$key - 2]) && !empty($values[$key - 3])) {
-                if (
-                    $values[$key - 1]['average'] - $values[$key - 2]['average'] > 0 &&
-                    $values[$key - 2]['average'] - $values[$key - 3]['average'] > 0
-                ) {
-                    $direction = 1;
-                } elseif (
-                    $values[$key - 1]['average'] - $values[$key - 2]['average'] < 0 &&
-                    $values[$key - 2]['average'] - $values[$key - 3]['average'] < 0
-                ) {
-                    $direction = 0;
-                } else {
-                    continue;
-                }
-            } else {
-                continue;
-            }
-*/
-$direction = rand(0,1);
-
-if ($lastLocalLow > $previousLocalLow) {
-    $direction = 1;
-} elseif ($lastLocalHigh < $previousLocalHigh) {
-    $direction = 0;
-} else {
-    continue;
-}
-
-
-
-//            $direction = 1;/* temp hack */
+            $direction = rand(0,1);
             // buy
             if ($direction) {
-                $trade = $value['average'] + 3;
-                $trade = $value['average'];
+                $trade = $value['average'] + $spread;
             // sell
             } else {
-                $trade = $value['average'] - 3;
-                $trade = $value['average'];
+                $trade = $value['average'] - $spread;
             }
             continue;
         }
 
+        // check if price moved and closed transaction with profit or loss
         if ($direction) {
             if ($value['high'] > $trade + ($ratio * $size)) {
                 $wallet += $size * $ratio;
@@ -204,13 +157,12 @@ if ($lastLocalLow > $previousLocalLow) {
                 $trade = false;
             }
         }
-//        echo $wallet . PHP_EOL;
     }
-//    echo 'For ratio ' . $ratio . ' wallet final: ' . $wallet . ', wallet min: ' . $walletMin . ', wallet max: ' . $walletMax . PHP_EOL;
     $results['ratio_' . $ratio]['final'][] = $wallet;
     $results['ratio_' . $ratio]['min'][] = $walletMin;
     $results['ratio_' . $ratio]['max'][] = $walletMax;
 }
+
 echo PHP_EOL;
 echo '======================' . PHP_EOL;
 echo '==SIMULATION RESULTS==' . PHP_EOL;
